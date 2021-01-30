902e51c4fafc97d68d9b2306f5930c400f3111bf
jopms <joao.34@hotmail.com>
2021-01-30 12:24:05 +0000

    Added event listener to listen to transformations on css and to delete the class playing right after the trasnformation has been completed

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,7 +1,22 @@
 window.addEventListener('keypress', playSound);
 
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+function removeTransition (e){
+  this.classList.remove("playing");
+}
+
+
 function playSound (e){
   let audio = document.getElementById((e.key).toLowerCase());
+  
+  let key = document.querySelector(`.${(e.key).toLowerCase()}`);
+  if(!audio){
+    return;
+  }
   audio.currentTime = 0;  
   audio.play();
+  key.classList.add("playing");
 }
